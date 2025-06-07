8f8db7ece2351e76e42f944db3e5bfbc8eed53fd
Saicharan2708 <bojjasaicharan27@gmail.com>
2025-06-07 11:33:17 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// let arr=Array(9).fill(null);
-// let current="X";
-
-// function MyGame(blk){
-//     const id=Number(blk.id);
-//     if(arr[id]!==null)
-//         return;
-//     arr[id]=current;
-//     blk.innerText=current;
-//     checkWinner();
-//     current=current==="X"?"O":"X";
-// }
-// function checkWinner(){
-//     if(
-//         (arr[0]!==null && arr[0]==arr[1] && arr[1]==arr[2]) ||
-//         (arr[3]!==null && arr[3]==arr[4] && arr[4]==arr[5]) ||
-//         (arr[6]!==null && arr[6]==arr[7] && arr[7]==arr[8]) ||
-//         (arr[0]!==null && arr[0]==arr[3] && arr[3]==arr[6]) ||
-//         (arr[1]!==null && arr[1]==arr[4] && arr[4]==arr[7]) ||
-//         (arr[2]!==null && arr[2]==arr[5] && arr[5]==arr[8]) ||
-//         (arr[0]!==null && arr[0]==arr[4] && arr[4]==arr[8]) ||
-//         (arr[6]!==null && arr[6]==arr[4] && arr[4]==arr[2]) 
-//     ){
-//         document.writeln(`Winner is : ${current}`);
-//         return;
-//     }
-//     if(!arr.some(e=>e===null))
-//         document.writeln('It`s a Draw!!!')
-
-// }
 let arr = Array(9).fill(null);
 let current = "X";
 let gameOver = false;
